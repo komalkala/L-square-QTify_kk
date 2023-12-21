e3e1cc85f7b54ebed98b25181e2cef5ee53de4b1
komalkala <komalkala42@gmail.com>
2023-12-21 12:43:40 +0530

    Hi

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
+
+function App() {
+  const [searchData, useSearchData] = useState();
+  return (
+   <>
+<Navbar searchData={searchData} />
+<Hero />
+   </>
+  );
+}
+
+export default App;
+ 
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./Search.module.css"
+import { ReactComponent as SearchIcon } from "../../assets/Search_icon.svg";
+function Search({ placeholder }){
+    const onSubmit = (e) => {
+        e.preventDefault();
+    }
+    return(
+        <form className={styles.wrapper} onSubmit={onSubmit}>
+<input className={styles.search} placeholder={placeholder} required/> 
+<button type="submit" className={styles.searchButton} >
+    <SearchIcon/>
+</button>
+        </form>
+    )
+}
+
+export default Search;
